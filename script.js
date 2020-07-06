75d27d954dd747f57b7604dd5b075c042cb6c7b1
SimpleBeat <simplebeat@simplebeat.com>
2020-07-06 13:56:30 +0300

    hook quote-fetching api to html interface

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const quoteBtn = document.getElementById('new-quote')
+
 // Get Quote From API
 async function getQuote() {
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
@@ -5,10 +11,10 @@ async function getQuote() {
     try {
         const response = await fetch(proxyUrl + apiUrl)
         const data = await response.json()
-        console.log(data)
+        authorText.innerText = data.quoteAuthor
+        quoteText.innerText = data.quoteText
     } catch (error) {
         getQuote()
-        console.log('No quote', error)
     }
 }
 
